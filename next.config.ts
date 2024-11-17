879bfa276c20a017d2076819e1e9aa500fa417ad
ASDRUBALPACHECO <143983683+ASDRUBALPACHECO@users.noreply.github.com>
2024-11-17 18:48:09 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // Asegúrate de que Next.js use el puerto correcto
+
   serverRuntimeConfig: {
     port: process.env.PORT || 8018
   },
